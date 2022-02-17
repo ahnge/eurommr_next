fb54c0095773e44be5db130b2e13910b35e751b2
Ahnge <ahnge226@gmail.com>
2022-02-17 23:41:54 +0630

    jjj

--- pages/projects/[slogan].js
+++ pages/projects/[slogan].js
@@ -3,25 +3,6 @@ import Nav from "../../components/Nav";
 import Link from "next/link";
 import Projects from "../../components/Projects/Projects";
 
-const ProjectPage = ({ data }) => {
-  return (
-    <div className=" bg-lb2 dark:bg-db2 min-h-screen w-full lg:pl-64 xlr:pl-[400px] py-10 transition">
-      <Nav />
-      <DL />
-      <Projects data={data} />
-      <div className="px-10 flex justify-end mt-10">
-        <Link href={"/"}>
-          <a className="px-6 py-2 bg-yellow-400 rounded-md capitalize shadow-md text-black border-2 border-yellow-400 hover:border-black dark:border-yellow-400 hover:bg-db1 hover:text-white transition">
-            Back
-          </a>
-        </Link>
-      </div>
-    </div>
-  );
-};
-
-export default ProjectPage;
-
 export const getStaticPaths = async () => {
   const res = await fetch("https://strapi-eurommr.herokuapp.com/api/services");
   const resObj = await res.json();
@@ -52,3 +33,22 @@ export const getStaticProps = async (context) => {
     },
   };
 };
+
+const ProjectPage = ({ data }) => {
+  return (
+    <div className=" bg-lb2 dark:bg-db2 min-h-screen w-full lg:pl-64 xlr:pl-[400px] py-10 transition">
+      <Nav />
+      <DL />
+      <Projects data={data} />
+      <div className="px-10 flex justify-end mt-10">
+        <Link href={"/"}>
+          <a className="px-6 py-2 bg-yellow-400 rounded-md capitalize shadow-md text-black border-2 border-yellow-400 hover:border-black dark:border-yellow-400 hover:bg-db1 hover:text-white transition">
+            Back
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectPage;
