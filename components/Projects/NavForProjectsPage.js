2b61e9f043089dfe61833eed07fa8c234c58ec37
Ahnge <ahnge226@gmail.com>
2022-02-28 15:38:33 +0630

    back to projects section

--- components/Projects/NavForProjectsPage.js
+++ components/Projects/NavForProjectsPage.js
@@ -36,7 +36,7 @@ const NavForProjectsPage = ({ data }) => {
           );
         })}
         <li className=" w-full">
-          <Link href="/" onClick={() => setIsOpen(false)}>
+          <Link href="/#projects" onClick={() => setIsOpen(false)}>
             <div className="font-light text-base leading-[26px] py-3 block w-full border-l-8  transition-all pl-14 xlr:py-5 xlr:tracking-wide  hover:bg-hover hover:border-yellow-400 hover:text-white hover:dark:text-yellow-400 cursor-pointer bg-transparent border-transparent text-text_wm dark:text-text_dm">
               Back Home
             </div>
